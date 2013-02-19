9064ab56798932b538eed0b96e97603c9b6c2558
jakub.vano <jakub.vano@VANO-J.gdneurope.com>
2013-02-19 12:04:31 +0100

    change1

--- GIT-POKUS/src/Class1.java
+++ GIT-POKUS/src/Class1.java
@@ -1,4 +1,15 @@
 
 public class Class1 {
 
+	
+	
+	public Class1() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("123");
+	}
+	
+	
 }
